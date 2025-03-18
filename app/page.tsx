2dfb00cb9abd78c4bc0680db402f2a8bb866d1d9
ChrisKnowsDev <chrisharley81@gmail.com>
2025-03-18 18:44:58 -0400

    fixed type... test deploy

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="m-8">
-      <h1>Chris's New Site</h1>
+      <h1 className="text-7xl font-bold mb-4">Chris&apos;s New Site</h1>
       <p>Get all the latest news here</p>
-      <Button>Newsletter Signup</Button>
+      <Button className="mt-8">Newsletter Signup</Button>
     </div>
   );
 }
