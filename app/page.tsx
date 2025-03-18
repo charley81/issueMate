365ee94537e5d08ece5eb8e360d106676481ed14
ChrisKnowsDev <chrisharley81@gmail.com>
2025-03-18 18:41:42 -0400

    homepage edit, test deploy

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="m-8"><Button>Submit</Button></div>
+    <div className="m-8">
+      <h1>Chris's New Site</h1>
+      <p>Get all the latest news here</p>
+      <Button>Newsletter Signup</Button>
+    </div>
   );
 }
