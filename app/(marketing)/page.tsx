bc37eba60882007be2bfb15d232aa953c6b33fb7
ChrisKnowsDev <chrisharley81@gmail.com>
2025-03-20 22:16:25 -0400

    initial client side routes complete

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,9 @@
 import { Button } from '@/components/ui/button'
 
-export default function Home() {
+export default function HomePage() {
   return (
     <div className="m-8">
-      <h1 className="text-6xl font-black">Chris&apos;s New Site</h1>
-      <p>Get all the latest news here</p>
+      <h1 className="text-6xl font-black">marketing page...</h1>
       <Button className="mt-4">Newsletter Signup</Button>
     </div>
   )
