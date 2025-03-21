6e78eeb34e82bb936ef9245c93a9adcf2f9d47d1
ChrisKnowsDev <chrisharley81@gmail.com>
2025-03-20 21:12:02 -0400

    added basic styling to index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
     <div className="m-8">
-      <h1>Chris's New Site</h1>
+      <h1 className="text-6xl font-black">Chris&apos;s New Site</h1>
       <p>Get all the latest news here</p>
-      <Button>Newsletter Signup</Button>
+      <Button className="mt-4">Newsletter Signup</Button>
     </div>
-  );
+  )
 }
